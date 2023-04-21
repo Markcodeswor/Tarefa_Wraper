cb25ca023bc79f8af87040faa001c708e15c86f0
Markcodeswor <113196496+marcos-de-santana@users.noreply.github.com>
2023-04-21 17:52:00 -0300

    Initial commit

---
+++ src/br/com/marcos/convertvalor.java
@@ -0,0 +1,20 @@
+package br.com.marcos;
+
+public class convertvalor {
+    public static void main(String args []) {
+
+        int idade = 2;
+
+        System.out.print("O valor correspondente a idade do int é \n " + idade);
+
+        String valor = Integer.toString(idade);
+
+        System.out.print("\n O valor convertido Wraper é \n " + idade);
+
+
+
+    }
+
+
+
+}
